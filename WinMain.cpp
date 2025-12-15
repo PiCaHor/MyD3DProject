3667257bcd508ba406c117ae822db33142867d1b
Peach <948459471@qq.com>
2025-12-15 15:51:10 +0800

    Create repository
    Create repository

---
+++ WinMain.cpp
@@ -0,0 +1,66 @@
+#include "Chapter4/originApp.h"
+
+#include <shellapi.h> // for CommandLineToArgvW
+#include <wchar.h>    // for wcscmp
+
+using namespace DirectX;
+
+struct cmdLineInfo
+{
+	/*
+	choose a instance to run
+	0:initialize app
+	*/
+	int runInstanceIndex = -1;
+};
+
+bool processCmdLine(cmdLineInfo& cmd_line_info)
+{
+	int argc;
+	wchar_t** argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+	if (argv == NULL) {
+		MessageBox(nullptr, L"cmdLine error", L"Error", MB_OK);
+		return false;
+	}
+
+	for (int i = 1; i < argc; ++i) {
+		if (wcscmp(argv[i], L"-ins") == 0 && i + 1 < argc) {
+			cmd_line_info.runInstanceIndex = _wtoi(argv[i + 1]);
+			i++;
+		}
+	}
+
+	LocalFree(argv);
+
+	return true;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	cmdLineInfo m_cmd_line_info;
+
+	if (!processCmdLine(m_cmd_line_info)) return 1;
+
+	try
+	{
+		if (m_cmd_line_info.runInstanceIndex == 0)
+		{
+			InitDirect3DApp theApp(hInstance);
+			if (!theApp.Initialize()) return 0;
+
+			return theApp.Run();
+		}
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+
+	return 0;
+}
